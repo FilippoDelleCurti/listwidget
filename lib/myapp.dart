710337ecf07bc46cbe8294ab99d8ab6dfb42484a
filippo.dellecurti <filippo.dellecurti2004@liceopascal.com>
2023-03-31 11:43:46 +0200

    sostituire nomi liste

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
   @override
@@ -17,23 +18,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<String> items = [];
+  List<String> p1 = [];
+  List<String> p2 = [];
+  List<String> p3 = [];
 
   TextEditingController controller = TextEditingController();
 
-  void addItemToList() {
+  void addItemToListP1() {
     setState(() {
-      items.add(controller.text);
+      p1.add(controller.text);
       _makeList();
       controller.clear();
     });
   }
 
-  /*Future<void> _loadCounter() async {
-   final prefs = await SharedPreferences.getInstance();
-    setState(() {items = prefs.getStringList('items')?[];});
+  void addItemToListP2() {
+    setState(() {
+      p2.add(controller.text);
+      _makeList();
+      controller.clear();
+    });
   }
-*/
+
+  void addItemToListP3() {
+    setState(() {
+      p3.add(controller.text);
+      _makeList();
+      controller.clear();
+    });
+  }
+
   @override
   void initState() {
     // TODO: implement initState
@@ -44,52 +58,191 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<void> _loadList() async {
     final prefs = await SharedPreferences.getInstance();
     setState(() {
-       items = (prefs.getStringList('list') ?? []);
+      p1 = (prefs.getStringList('listP1') ?? []);
+      p2 = (prefs.getStringList('listP2') ?? []);
+      p3 = (prefs.getStringList('listP3') ?? []);
     });
   }
 
   Future<void> _makeList() async {
     final prefs = await SharedPreferences.getInstance();
     setState(() {
-      prefs.setStringList('list', items);
+      prefs.setStringList('listP1', p1);
+      prefs.setStringList('listP2', p2);
+      prefs.setStringList('listP3', p3);
     });
   }
 
+//sostituire nomi liste
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Materie'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: ListView.builder(
-              itemCount: items.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text('${items[index]}'),
-                );
-              },
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.directions_car)),
+              Tab(icon: Icon(Icons.directions_transit)),
+              Tab(icon: Icon(Icons.directions_bike)),
+            ],
+          ),
+          title: const Text('Tabs Demo'),
+        ),
+        body: TabBarView(
+          children: [
+            Column(
+              children: <Widget>[
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text('${items[index]}'),
+                      );
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: controller,
+                    decoration: InputDecoration(
+                      hintText: 'Inserisci la materia che desideri studiare',
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: addItemToList,
+                  child: Text('Aggiungi materia'),
+                ),
+              ],
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: controller,
-              decoration: InputDecoration(
-                hintText: 'Inserisci la materia che desideri studiare',
-              ),
+            Column(
+              children: <Widget>[
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text('${items[index]}'),
+                      );
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: controller,
+                    decoration: InputDecoration(
+                      hintText: 'Inserisci la materia che desideri studiare',
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: addItemToList,
+                  child: Text('Aggiungi materia'),
+                ),
+              ],
             ),
-          ),
-          ElevatedButton(
-            onPressed: addItemToList,
-            child: Text('Aggiungi materia'),
-          ),
-        ],
+            Column(
+              children: <Widget>[
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text('${items[index]}'),
+                      );
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: controller,
+                    decoration: InputDecoration(
+                      hintText: 'Inserisci la materia che desideri studiare',
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: addItemToList,
+                  child: Text('Aggiungi materia'),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
 //vedi tabview
+
+/*void main() {
+  runApp(const TabBarDemo());
+}
+
+class TabBarDemo extends StatelessWidget {
+  const TabBarDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_transit)),
+                Tab(icon: Icon(Icons.directions_bike)),
+              ],
+            ),
+            title: const Text('Tabs Demo'),
+          ),
+          body: const TabBarView(
+            children: [
+              Icon(Icons.directions_car),
+              Icon(Icons.directions_transit),
+              Icon(Icons.directions_bike),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+ */
+
+/*Column(
+children: <Widget>[
+Expanded(
+child: ListView.builder(
+itemCount: items.length,
+itemBuilder: (context, index) {
+return ListTile(
+title: Text('${items[index]}'),
+);
+},
+),
+),
+Padding(
+padding: const EdgeInsets.all(16.0),
+child: TextField(
+controller: controller,
+decoration: InputDecoration(
+hintText: 'Inserisci la materia che desideri studiare',
+),
+),
+),
+ElevatedButton(
+onPressed: addItemToList,
+child: Text('Aggiungi materia'),
+),
+],
+),
+*/
